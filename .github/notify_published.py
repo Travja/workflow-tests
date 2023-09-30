7bbbf698607dcdc04c57bddbf0bde2e63827422c
Travja <the.only.t.craft@gmail.com>
2023-09-30 11:28:15 -0600

    Initial commit

---
+++ .github/notify_published.py
@@ -0,0 +1,44 @@
+import re
+import requests
+import simplejson as json
+import sys
+
+is_dev = len(sys.argv) >= 3 and bool(sys.argv[2])
+search_string = \
+    r'Uploaded to (ossrh|central): (https:\/\/s01\.oss\.sonatype\.org(:443)?\/.*?\/com\/promcteam\/(.*?)\/(.*?)\/(' \
+    r'.*?)(?<!sources|javadoc)\.jar(?!\.asc)) '
+
+
+def get_info():
+    with open('log.txt', 'r') as file:
+        content = file.read()
+        data = re.findall(search_string, content, re.MULTILINE)
+        found_version = data[0][5]
+        artifact_id = data[0][3]
+        artifact_url = data[0][1]
+        return found_version, artifact_id, artifact_url
+
+
+version, name, url = get_info()
+split = version.split('-')[0:-2]
+version = '-'.join(split)
+if not is_dev:
+    url = re.sub(
+        'https:\/\/s01\.oss\.sonatype\.org:443\/service\/local\/staging\/deployByRepositoryId\/compromcteam-\d+',
+        'https://s01.oss.sonatype.org/service/local/repositories/releases/content',
+        url)
+embed = {
+    'author': {
+        'name': 'New ' + ('Dev ' if is_dev else '') + 'Build Available!',
+        'url': 'https://github.com/promcteam/' + name
+    },
+    'title': version,
+    'description': 'Click the link above to download the new build!',
+    'url': url,
+    'color': 5341129
+}
+
+requests.post(sys.argv[1],
+              headers={'Content-Type': 'application/json'},
+              data=json.dumps({'embeds': [embed]})
+              )
---
+++ src/main/java/me/travja/Main.java
@@ -0,0 +1,7 @@
+package me.travja;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
